88b7d19e5af5778414d0907128fc108b193527b9
Mariam <mariamomri.dev@gmail.com>
2026-04-27 20:53:05 +0200

    create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import logo from "../assets/img/logo.png";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="bg-transparent text-white p-4 flex items-center justify-between">
+      <img src={logo} alt="Logo" className="h-50" />
+      <nav className="mt-4">
+        <ul className="flex space-x-4 text-lg">
+          <li>
+            <a href="#" className="hover:underline">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <button href="#" className="mr-15 text-lg">
+        Login
+      </button>
+    </header>
+  );
 }
 
 export default Header;
