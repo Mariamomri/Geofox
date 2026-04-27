2f7a7c44d6e616078441fad1724a4e094746663c
Mariam <mariamomri.dev@gmail.com>
2026-04-27 22:21:31 +0200

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import audio from "../assets/audio/terra.mp3";
 
 function Footer() {
-  return <div>Footer</div>;
+  const data = new Date();
+
+  return (
+    <div className="bg-[#F1F3F4] text-white p-4 flex items-center justify-between fixed bottom-0 w-full">
+      <div className="text-black">
+        <p>{data.toLocaleDateString("it-IT")}</p>
+        <p> {data.toLocaleTimeString("it-IT")}</p>
+      </div>
+
+      <audio controls src={audio} className="color-move" />
+
+      <p className="text-black">
+        © 2026 Cfitech- Mariam Omri <br />
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
