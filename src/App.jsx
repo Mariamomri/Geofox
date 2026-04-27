6af1b05e4dffea6536f831e4a1e2a1aa9c7e2414
Mariam <codegirlbxl@gmail.com>
2026-04-27 20:14:31 +0200

    add video to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // import { useState } from 'react'
+import Header from "./components/Header";
+import Home from "./pages/Home";
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <Home />
       <h1>Geofox</h1>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const videoTerra = `${import.meta.env.BASE_URL}videos/terra.mp4`;
+
+  return (
+    <div>
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover -z-2"
+      >
+        <source src={videoTerra} type="video/mp4" />
+        Ton navigateur ne supporte pas la vidéo HTML5.
+      </video>
+    </div>
+  );
 }
 
 export default Home;
