a6a76fb2d259375feeef39ffef88420e62970df3
Mariam <mariamomri.dev@gmail.com>
2026-05-05 13:13:22 +0200

    add basename into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Geofox">
+      {/*il basename fa ignorare lo Geofox dentro dell'url e cerca / per home oppure /quiz perche dentro a vite.config.js ho dichiarato base: "/Geofox/"*/}
       <App />
     </BrowserRouter>
   </StrictMode>,
