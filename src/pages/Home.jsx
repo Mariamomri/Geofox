06b3116094fa0d5fb67aee73cc914ffc81fc09ec
Mariam <codegirlbxl@gmail.com>
2026-05-05 18:49:53 +0200

    add video to ModeDeJeu.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Home() {
   const videoTerra = `${import.meta.env.BASE_URL}videos/terra.mp4`;
   const videoSole = `${import.meta.env.BASE_URL}videos/sole.mp4`;
-  // const videoLune = `${import.meta.env.BASE_URL}videos/terra2.mp4`;
+
 
   return (
     <>
@@ -40,22 +40,6 @@ function Home() {
           />
           Ton navigateur ne supporte pas la vidéo HTML5.
         </video>
-
-        {/* video colori */}
-        {/* <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="absolute inset-0 w-full  object-cover -z-2"
-        >
-          <source
-            src={videoLune}
-            type="video/mp4"
-            className="hidden hover:hidden-none"
-          />
-          Ton navigateur ne supporte pas la vidéo HTML5.
-        </video>*/}
 
         <div className=" divtext text-left px-20">
           <h1 className="titolo">GEOFOX</h1>
