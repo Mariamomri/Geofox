d031b9ba6e549bae6c6dbd78207ef016d9a7ac2d
Mariam <codegirlbxl@gmail.com>
2026-05-05 12:53:06 +0200

    define page routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Quiz from "./pages/Quiz";
+import Resultats from "./pages/Resultats";
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/resultats" element={<Resultats />} />
+      </Routes>
       <Footer />
     </>
   );
