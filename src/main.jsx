153ab0ececaa72c9f405e24b51a3c65c3bff6e82
Mariam <mariamomri.dev@gmail.com>
2026-05-05 15:08:44 +0200

    create page ModeDeJeu.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/Geofox">
-      {/*il basename fa ignorare lo Geofox dentro dell'url e cerca / per home oppure /quiz perche dentro a vite.config.js ho dichiarato base: "/Geofox/"*/}
+      {/*il basename fa ignorare lo Geofox dentro dell'url e cerca "/ per home" oppure "/quiz" perche dentro a vite.config.js ho dichiarato base: "/Geofox/"*/}
       <App />
     </BrowserRouter>
   </StrictMode>,
