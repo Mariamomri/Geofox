02cb222640e7e337a1bf8fc26d2e482e2136063a
Mariam <codegirlbxl@gmail.com>
2026-05-05 18:32:49 +0200

    add text into ModeDeJeu.jsx

--- src/pages/ModeDeJeu.jsx
+++ src/pages/ModeDeJeu.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function ModeDeJeu() {
-  return <div>ModeDeJeu</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen text-white">
+        <h1 className="text-4xl font-bold mb-4">Choisis ton mode de jeu</h1>
+        <p className="text-white/60 mb-8">Quel défi veux-tu relever ?</p>
+      </div>
+    </>
+  );
 }
 
 export default ModeDeJeu;
