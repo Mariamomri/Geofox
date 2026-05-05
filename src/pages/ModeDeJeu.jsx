206eb747dbc85620d991f2ecedfa1adf6bb4686c
Mariam <mariamomri.dev@gmail.com>
2026-05-05 18:49:53 +0200

    add video to ModeDeJeu.jsx

--- src/pages/ModeDeJeu.jsx
+++ src/pages/ModeDeJeu.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 
 function ModeDeJeu() {
+  const videoLune = `${import.meta.env.BASE_URL}videos/terra2.mp4`;
   return (
     <>
+      {/* video colori */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full object-cover -z-2"
+      >
+        <source
+          src={videoLune}
+          type="video/mp4"
+          className="hidden hover:hidden-none"
+        />
+        Ton navigateur ne supporte pas la vidéo HTML5.
+      </video>
+
       <div className="flex flex-col items-center justify-center min-h-screen text-white">
         <h1 className="text-4xl font-bold mb-4">Choisis ton mode de jeu</h1>
         <p className="text-white/60 mb-8">Quel défi veux-tu relever ?</p>
