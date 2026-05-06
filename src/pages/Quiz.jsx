ced060597febe240ef1f2dd58f452ffe0f79f206
Mariam <codegirlbxl@gmail.com>
2026-05-06 11:46:07 +0200

    add video into Quiz.jsx

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -3,6 +3,7 @@ import { useQuiz } from "../context/QuizContext.jsx";
 
 function Quiz() {
   const { mode } = useQuiz();
+  const videoTerra = `${import.meta.env.BASE_URL}videos/terra.mp4`;
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-white">
@@ -10,6 +11,21 @@ function Quiz() {
       <p>
         Mode choisi : <span className="font-bold">{mode}</span>
       </p>
+
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover -z-2"
+      >
+        <source
+          src={videoTerra}
+          type="video/mp4"
+          className="hidden hover:hidden-none"
+        />
+        Ton navigateur ne supporte pas la vidéo HTML5.
+      </video>
     </div>
   );
 }
