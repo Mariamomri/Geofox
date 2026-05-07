34e8a0a95e932b9da43d66c09759be57fc8dbfc4
Mariam <mariamomri.dev@gmail.com>
2026-05-08 01:32:16 +0200

    set quiz to 10 questions

--- back_end/api/quiz.php
+++ back_end/api/quiz.php
@@ -7,23 +7,28 @@ header('Content-Type: application/json');
 require_once '../classes/bd.php';
 require_once '../classes/Pay.php';
 
-$pays = getRandomPays(4);
+$n = isset($_GET['n']) ? (int)$_GET['n'] : 10;
 
-$bonPays = $pays[0];
+$questions = [];
 
-$question = [
-  'type'      => 'capital',
-  'statement' => 'Quelle est la capitale de ' . $bonPays['name'] . ' ?',
-  'answer'    => $bonPays['capital'],
-  'flag'      => $bonPays['flag'],
-  'options'   => [
-    $pays[0]['capital'],
-    $pays[1]['capital'],
-    $pays[2]['capital'],
-    $pays[3]['capital'],
-  ]
-];
+for ($i = 0; $i < $n; $i++) {
+  // 4 pays au hasard
+  $pays = getRandomPays(4);
+  $bonPays = $pays[0];
 
-echo json_encode($question, JSON_PRETTY_PRINT); //Trasforma l'array PHP in una stringa JSON che React può leggere. React riceve questa stringa JSON e la converte in un oggetto JavaScript per visualizzarla all'utente.
+  $questions[] = [
+    'statement' => 'Quelle est la capitale de ' . $bonPays['name'] . ' ?',
+    'answer'    => $bonPays['capital'],
+    'flag'      => $bonPays['flag'],
+    'options'   => [
+      $pays[0]['capital'],
+      $pays[1]['capital'],
+      $pays[2]['capital'],
+      $pays[3]['capital'],
+    ]
+  ];
+}
+
+echo json_encode($questions, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE); //Trasforma l'array PHP in una stringa JSON che React può leggere. React riceve questa stringa JSON e la converte in un oggetto JavaScript per visualizzarla all'utente.
 
 /*React fa una richiesta fetch all'endpoint PHP. PHP legge il database con PDO, crea un array con la domanda e le 4 opzioni, e lo invia in formato JSON con json_encode. React riceve il JSON e lo mostra all'utente*/
