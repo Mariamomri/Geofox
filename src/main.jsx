a2da52a729a56a9c2c14708d1f6b442c63a69799
Mariam <mariamomri.dev@gmail.com>
2026-07-26 00:59:09 +0200

    fix router basename for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { QuizProvider } from "./context/QuizContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Geofox">
+    <BrowserRouter>
       {/*il basename fa ignorare lo Geofox dentro dell'url e cerca "/ per home" oppure "/quiz" perche dentro a vite.config.js ho dichiarato base: "/Geofox/"*/}
       <QuizProvider>
         <App />
